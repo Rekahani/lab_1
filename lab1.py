e8d8be3f18d055774ab0f7ee48a42ff260827f9f
Reka Hani Latifah Nurhasanah <rekahanilatifahnurhasanah2404@gmail.com>
2020-10-30 12:34:32 +0700

    file biodata

---
+++ lab1.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+print("==========================")
+print("BIODATA")
+print("==========================")
+
+
+
+nama= input ("masukkan nama: ")
+nama2= input ("nama panggilan: ")
+npm= input ("nim saya: ")
+tl= input ("tempat lahir: ")
+tgl= input ("tanggal lahir: ")
+almt= input ("masukkan alamat: ")
+tlpn= input ("masukkan nomor telepon: ")
+
+
+
+#print
+
+
+print("============================")
+print("BIODATA SAYA")
+print("============================")
+
+
+print("hallo nama saya",nama,"nama panggilan saya",nama2,"nim saya adalah",npm,"tempat lahir saya di kota",tl)
+print("tanggal lahir saya adalah",tgl,"alamat saya",almt," dan nomor telepon saya",tlpn)
+
+

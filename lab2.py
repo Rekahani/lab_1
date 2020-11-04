4231a7813fb39b08fec652470669e52052776b95
Reka Hani Latifah Nurhasanah <rekahanilatifahnurhasanah2404@gmail.com>
2020-11-04 19:44:30 +0700

    menambahkan file baru

---
+++ lab2.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  4 19:09:11 2020
+
+@author: LENOVO i5
+"""
+
+a = eval(input("masukkan nilai a: "))
+b = eval(input("masukkan nilai b: "))
+
+
+hasil = a + b
+hasil1 = a / b
+print("variable a=", a)
+print("variable b=", b)
+print("hasil penggabungan %d + %d = %d" %(a,b,hasil1))
+
+
+print()
+print("             #KONVERSI NILAI VARIABLE")
+print()
+
+
+a=int(a)
+b=int(b)
+print("hasil penjumlahan %d + %d = %d" %(a,b,hasil))
+print("hasil pembagian %d / %d = %d" %(a,b,hasil1))
+
+
+
+
